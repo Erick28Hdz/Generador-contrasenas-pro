b16ba2cc3dd286e4f13a88acbde56f8cb345cf35
Erick Hernandez <erick.hdz9628@gmail.com>
2025-05-13 22:27:32 -0500

    service

--- public/js/service-worker.js
+++ public/js/service-worker.js
@@ -3,17 +3,17 @@ self.addEventListener('install', e => {
   e.waitUntil(
     caches.open('pwacache').then(cache => {
       return cache.addAll([
-        '/Generador-contrasenas-pro/index.html',
-        '/Generador-contrasenas-pro/css/style.css',
-        '/Generador-contrasenas-pro/js/auth.js',
-        '/Generador-contrasenas-pro/js/main.js',
-        '/Generador-contrasenas-pro/js/bloqueo.js',
-        '/Generador-contrasenas-pro/js/cifrado.js',
-        '/Generador-contrasenas-pro/js/prueba.js',
-        '/Generador-contrasenas-pro/js/sheets.js',
-        '/Generador-contrasenas-pro/manifest.json',
-        '/Generador-contrasenas-pro/image/icon-256.png',
-        '/Generador-contrasenas-pro/image/icon-512.png'
+        '/Generador-contrasenas-pro/public/index.html',
+        '/Generador-contrasenas-pro/public/css/style.css',
+        '/Generador-contrasenas-pro/public/js/auth.js',
+        '/Generador-contrasenas-pro/public/js/main.js',
+        '/Generador-contrasenas-pro/public/js/bloqueo.js',
+        '/Generador-contrasenas-pro/public/js/cifrado.js',
+        '/Generador-contrasenas-pro/public/js/prueba.js',
+        '/Generador-contrasenas-pro/public/js/sheets.js',
+        '/Generador-contrasenas-pro/public/manifest.json',
+        '/Generador-contrasenas-pro/public/image/icon-256.png',
+        '/Generador-contrasenas-pro/public/image/icon-512.png'
       ]);
     })
   );
